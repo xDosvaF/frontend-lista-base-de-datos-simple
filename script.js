24a1a7d1ca35740015a426be661a84f111e4569d
xDosva <oddynucleth@gamil.com>
2026-06-28 17:32:40 -0600

    Lista de productos con una base de datos simple

--- script.js
+++ script.js
@@ -1,58 +1,90 @@
-// Variables del DOM
-let textBox = document.querySelector("#text-box")
-const btnSave = document.querySelector("#button-save")
-let lsDinamica = document.querySelector("#lista-dinamica")
+// Variables del DOM 
+let txtProducto = document.querySelector("#text-producto");
+let cbbCategoria = document.querySelector("#combo-box-categoria")
+let numPrecio = document.querySelector("#number-precio")
+let btnGuardar = document.querySelector("#button-guardar")
+let lsPadre = document.querySelector("#lista-padre")
 
 // Variable Array
-let miArray = ["Esteban","Mauro","Antonio"]
+let listaProducto = []
 
-// Funcion para crear un boton para eiminar una lista o algo así
-function botonEliminar(lista,valor){
+// Variable localStorage
+let listaVieja = localStorage.getItem("producto")
+
+if(listaVieja !== null){
+    listaProducto = JSON.parse(listaVieja)
+    listarProductoExistente()
+}
+
+
+function crearButtonRemove(lista,producto){
     let btnRemove = document.createElement("button")
-    btnRemove.textContent = "X"
     btnRemove.className = "button-remove"
-    lista.appendChild(btnRemove)
+    btnRemove.textContent = "X"
     btnRemove.addEventListener("click",()=>{
-        let pos = miArray.indexOf(valor)
-        miArray.splice(pos)
+        let listaFilter = listaProducto.filter(listaProducto => listaProducto.producto !== producto)
+        localStorage.setItem("producto",JSON.stringify(listaFilter))
         lista.remove()
+    })
+    lista.appendChild(btnRemove)
+}
+
+function crearProducto(producto,categoria,precio){
+    let newProducto = {
+        "producto":producto,
+        "categoria":categoria,
+        "precio":precio
+    }
+    crearLista(producto,categoria,precio)
+    listaProducto.push(newProducto)
+    localStorage.setItem("producto",JSON.stringify(listaProducto))
+}
+
+function crearLista(producto,categoria,precio){
+    // Crear la lista hijo
+    let newLista = document.createElement("li")
+    newLista.className = "lista-hijo"
+
+    // Datos de la lista
+    let dataProducto = document.createElement("p")
+    dataProducto.textContent = producto
+
+    let dataCategoria = document.createElement("p")
+    dataCategoria.textContent = categoria
+
+    let dataPrecio = document.createElement("p")
+    dataPrecio.textContent = `Q ${precio}`
+
+    // Agregar los datos a la lista hijo
+    newLista.appendChild(dataProducto)
+    newLista.appendChild(dataCategoria)
+    newLista.appendChild(dataPrecio)
+
+    // Agregar la funcion de crear el boton para eliminar la lista
+    crearButtonRemove(newLista,producto)
+
+    // Agregar la lista hijo a la lista padre
+    lsPadre.appendChild(newLista)
+}
+
+function listarProductoExistente(){
+    listaProducto.forEach((Element)=>{
+        crearLista(Element.producto,Element.categoria,Element.precio)
     })
 }
 
-// Funcion para lista mi Array
-function leerArray(){
-    for(let i=0;i<miArray.length;i++){
-        let newLista = document.createElement("li")
-        newLista.textContent = miArray[i]
-        newLista.className = "lista-hijo"
-        lsDinamica.appendChild(newLista)
-        botonEliminar(newLista,miArray[i])
+btnGuardar.addEventListener("click",()=>{
+    if(txtProducto.value.trim() === ""){
+        return alert("Esribe un producto")
     }
-}
-
-leerArray()
-
-// Funciion para crear un nuevo Elemento
-function crearElemento(valor){
-    let newLista = document.createElement("li")
-    newLista.textContent = valor
-    newLista.className = "lista-hijo"
-    lsDinamica.appendChild(newLista)
-    miArray.push(valor)
-    botonEliminar(newLista)
-}
-
-// Evento click
-btnSave.addEventListener("click",()=>{
-    if(textBox.value.trim() === ""){
-        alert("¡Escribe algo joder!")
-    }else{
-        if(miArray.includes(textBox.value.trim())){
-            alert("El nombre ya existe")
-        }else{
-            alert("Se ha guardado correctamente")
-            crearElemento(textBox.value.trim())
-            textBox.value = ""
-        }
+    if(numPrecio.value.trim() == ""){
+        return alert("Debes de especificar el precio")
+    }
+    else{
+        alert("Se ha guardado correctamente")
+        crearProducto(txtProducto.value,cbbCategoria.value,numPrecio.value)
+        console.log(listaProducto)
+        txtProducto.value = ""
+        numPrecio.value = 0
     }
 })
